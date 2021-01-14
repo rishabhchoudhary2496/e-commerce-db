6e840edc3ac59d2d16011a861acf8226f25e2985
rishabh choudhary <76998509+rishabhchoudhary2496@users.noreply.github.com>
2021-01-14 18:32:14 +0530

    Add files via upload

---
+++ customerAddress.sql
@@ -0,0 +1,20 @@
+-- create table customerAddress (
+--     customer_address_id int primary key auto_increment,
+-- 	user_id int unique,
+-- 	country varchar(30),
+--     state varchar(30),
+--     city varchar(30),
+--     street varchar(50),
+--     landmark varchar(40),
+--     PIN int,
+--     check(length(PIN) = 6),
+--     foreign key(user_id) references user(user_id)
+-- )
+
+-- insert into customerAddress(user_id,country,state,city,street,landmark,PIN) values(1,'india','panjab','amritsar','some street','lorem',123456);
+-- insert into customerAddress(user_id,country,state,city,street,landmark,PIN) values(2,'india','delhi','amritsar','some street','lorem',123457);
+-- insert into customerAddress(user_id,country,state,city,street,landmark,PIN) values(3,'india','chandigarh','chandigarh','some street','lorem',123458);
+-- insert into customerAddress(user_id,country,state,city,street,landmark,PIN) values(4,'india','himachal','palampur','some street','lorem',123452);
+-- insert into customerAddress(user_id,country,state,city,street,landmark,PIN) values(5,'india','panjab','bhatinda','some street','lorem',123453);
+#select * from customerAddress
+
