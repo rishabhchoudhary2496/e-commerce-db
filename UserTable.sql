f08da6a8b9df4a74544f42600e53020fac50fd2b
rishabh choudhary <76998509+rishabhchoudhary2496@users.noreply.github.com>
2021-01-15 14:23:40 +0530

    Update UserTable.sql

--- UserTable.sql
+++ UserTable.sql
@@ -1,9 +1,11 @@
 -- create table user(
 --  user_id int primary key auto_increment,
 --  user_name varchar(30),
+--  address_id int,
 --  user_password varchar(50),
 --  user_phone bigint,
 --  check (length(user_phone)=10)
+--  foreign key(address_id) references customerAddress(customer_address_id)
 -- )
 
 -- insert into user(user_name,user_password,user_phone) values('Alfreds Futterkiste','12345678',9056784321);
