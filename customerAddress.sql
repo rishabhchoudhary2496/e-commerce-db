53bd0de8282d0caa92558b0b9ebff0d1fa27e25a
rishabh choudhary <76998509+rishabhchoudhary2496@users.noreply.github.com>
2021-01-15 14:24:55 +0530

    Update customerAddress.sql

--- customerAddress.sql
+++ customerAddress.sql
@@ -9,10 +9,10 @@
 --     check(length(PIN) = 6),
 -- )
 
--- insert into customerAddress(user_id,country,state,city,street,landmark,PIN) values(1,'india','panjab','amritsar','some street','lorem',123456);
--- insert into customerAddress(user_id,country,state,city,street,landmark,PIN) values(2,'india','delhi','amritsar','some street','lorem',123457);
--- insert into customerAddress(user_id,country,state,city,street,landmark,PIN) values(3,'india','chandigarh','chandigarh','some street','lorem',123458);
--- insert into customerAddress(user_id,country,state,city,street,landmark,PIN) values(4,'india','himachal','palampur','some street','lorem',123452);
--- insert into customerAddress(user_id,country,state,city,street,landmark,PIN) values(5,'india','panjab','bhatinda','some street','lorem',123453);
+-- insert into customerAddress(country,state,city,street,landmark,PIN) values('india','panjab','amritsar','some street','lorem',123456);
+-- insert into customerAddress(country,state,city,street,landmark,PIN) values('india','delhi','amritsar','some street','lorem',123457);
+-- insert into customerAddress(country,state,city,street,landmark,PIN) values('india','chandigarh','chandigarh','some street','lorem',123458);
+-- insert into customerAddress(country,state,city,street,landmark,PIN) values('india','himachal','palampur','some street','lorem',123452);
+-- insert into customerAddress(country,state,city,street,landmark,PIN) values('india','panjab','bhatinda','some street','lorem',123453);
 #select * from customerAddress
 
