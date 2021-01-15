8687dc40cd872fd3e2838feb5e30611040935779
rishabh choudhary <76998509+rishabhchoudhary2496@users.noreply.github.com>
2021-01-15 14:22:31 +0530

    Update customerAddress.sql

--- customerAddress.sql
+++ customerAddress.sql
@@ -1,14 +1,12 @@
 -- create table customerAddress (
 --     customer_address_id int primary key auto_increment,
--- 	user_id int unique,
--- 	country varchar(30),
+-- 	   country varchar(30),
 --     state varchar(30),
 --     city varchar(30),
 --     street varchar(50),
 --     landmark varchar(40),
 --     PIN int,
 --     check(length(PIN) = 6),
---     foreign key(user_id) references user(user_id)
 -- )
 
 -- insert into customerAddress(user_id,country,state,city,street,landmark,PIN) values(1,'india','panjab','amritsar','some street','lorem',123456);
